f235b64054a64f9174e897baffd7b36f50b760c8
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-08-26 00:55:16 +0200

    initial commit

---
+++ src/main/java/org/alliance/Doctor.java
@@ -0,0 +1,8 @@
+package org.alliance;
+
+public class Doctor {
+  public void doc(){
+    System.out.println("This is the doctor");
+  }
+
+}
---
+++ src/main/java/org/alliance/Main.java
@@ -0,0 +1,8 @@
+package org.alliance;
+
+public class Main {
+  public static void main(String[] args) {
+    Doctor alliance = new Doctor();
+    alliance.doc();
+  }
+}
