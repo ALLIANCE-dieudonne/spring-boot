1a23d0018c4238fbea2f9cf2e785aa9a066e2962
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-08-26 02:41:15 +0200

    java configuration

---
+++ src/main/java/org/alliance/BeanConfig.java
@@ -0,0 +1,10 @@
+package org.alliance;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan( basePackages = "org.alliance")
+public class BeanConfig {
+
+}
